5726bfe84b59ded929ff126956b768720f101d87
Luna-LinXreeN <tobias.pachlatko@gmx.at>
2023-10-16 10:57:56 +0200

    deleted consol logs

--- databaseConnection.php
+++ databaseConnection.php
@@ -9,11 +9,9 @@ global $conn;
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "<div><p class='databaseText'>Connected to Database</p></div>";
-    console.log("Conectend to DB")
+    echo "<div><p class='databaseText'>Connected to Database</p></div>"
 } catch (PDOException $e) {
     echo "<div><p class='errorMsg'> No Database connection: " . $e->getMessage()."</p></div>";
-    console.log("Conection Error")
 }
 
 ?>
